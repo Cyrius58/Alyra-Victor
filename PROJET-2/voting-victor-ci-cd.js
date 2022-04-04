f2eb98dfb53f481059d2ab1da0f0b76c09e897d5
Cyrius58 <45919232+Cyrius58@users.noreply.github.com>
2022-04-04 22:06:55 +0200

    Update voting-victor-ci-cd.js

--- PROJET-2/voting-victor-ci-cd.js
+++ PROJET-2/voting-victor-ci-cd.js
@@ -20,7 +20,7 @@ contract("Voting",accounts=>{
     //Instanciate the 'Voting' contract
     let votingInstance;
 
-    describe ("tests about voters :",function (){
+    describe ("Tests about voters :",function (){
         context("Set :",function(){
             beforeEach(async function (){
                 votingInstance=await Voting.new({from:owner});
@@ -104,7 +104,35 @@ contract("Voting",accounts=>{
             });
         });
     });
-
+    describe("Testing on user status:", function () {
+        before(async () => {
+            votingInstance = await Voting.new({from:owner});
+        });
+        context("onlyOwner:",function (){
+            it("...sould revert on addVoter", async () => {
+                await expectRevert(votingInstance.addVoter(second,          {from: second}), "Ownable: caller is not the owner");});
+            it("...sould revert on StartProposalRegistering", async () => {
+                await expectRevert(votingInstance.startProposalsRegistering({from: second}), "Ownable: caller is not the owner");});
+            it("...sould revert on endProposalsRegistering", async () => {
+                await expectRevert(votingInstance.endProposalsRegistering(  {from: second}), "Ownable: caller is not the owner");});
+            it("...sould revert on startVotingSession", async () => {
+                await expectRevert(votingInstance.startVotingSession(       {from: second}), "Ownable: caller is not the owner");});
+            it("...sould revert on endVotingSession", async () => {
+                await expectRevert(votingInstance.endVotingSession(         {from: second}), "Ownable: caller is not the owner");});
+            it("...sould revert on tallyVotes", async () => {
+                await expectRevert(votingInstance.tallyVotesDraw(           {from: second}), "Ownable: caller is not the owner");});
+        });
+        context("onlyVoters:",function (){
+            it("...sould revert on getVoter", async () => {
+                await expectRevert(votingInstance.getVoter(owner,{from: notRegistered}), "You're not a voter");});
+            it("...sould revert on getOneProposal", async () => {
+                await expectRevert(votingInstance.getOneProposal(0,{from: notRegistered}), "You're not a voter");});
+            it("...sould revert on addProposal", async () => {
+                await expectRevert(votingInstance.addProposal("test",{from: notRegistered}), "You're not a voter");});
+            it("...sould revert on setVote", async () => {
+                await expectRevert(votingInstance.setVote(0,{from: notRegistered}), "You're not a voter");});  
+        });
+    });
 
 
 
